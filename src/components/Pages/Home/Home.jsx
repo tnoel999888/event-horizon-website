61b618ffa4e91db24b88e5d8370ece6a42d9a751
Tim <tnoel7@msn.com>
2021-05-31 23:24:08 +0100

    Offsetting carousel image focal points to track subject on resize. Adding scroll bar to each page for consistent content horizontal positioning across all pages.

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,42 +1,25 @@
 import './home.css';
-import PressShot1 from '../../../assets/press-shot-1.jpg'
-import PressShot2 from '../../../assets/press-shot-2.jpg'
-import PressShot3 from '../../../assets/press-shot-3.jpg'
 import Carousel from 'react-bootstrap/Carousel'
 
 function Home() {
   return (
     <Carousel className="carousel">
       <Carousel.Item className="carousel-item">
-        <img
-          className="carousel-img"
-          src={PressShot2}
-          alt="First slide"
-        />
+        <div className="press-shot-3" />
         <Carousel.Caption>
           <h3>About:</h3>
           <p>25 year old DJ/Producer based in London, UK.<br/> Tech House/UK Bass/Lofi House/Garage/Drum & Bass/Dubstep.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="carousel-item">
-        <img
-          className="carousel-img"
-          src={PressShot1}
-          alt="Second slide"
-        />
-
+        <div className="press-shot-1" />
         <Carousel.Caption>
           <h3>Releases:</h3>
           <p>Night Bass, Monstercat, Circus Records, IN/ROTATION, Maraki Records, Crucast, Deeprot, The Wub Club, Saucy Records, Supersize, Strictly Flava, Yosh, 3000 Bass, Feuille Vierge, Gassed, Brøken, Project Allout Records, Pack London, Open Outlets, Articulate Sound, Certified.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="carousel-item">
-        <img
-          className="carousel-img"
-          src={PressShot3}
-          alt="Third slide"
-        />
-
+        <div className="press-shot-4" />
         <Carousel.Caption>
           <h3>Support From:</h3>
           <p>My Nu Leng, AC Slater, Phlegmatic Dogs, Matroda, Bleu Clair, Holy Goof, Notion, Skepsis, Darkzy + more.</p>
