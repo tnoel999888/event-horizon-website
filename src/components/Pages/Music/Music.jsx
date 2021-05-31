b84f00da1088f1d04ae39c56475872cba50b3234
Timan Noel <tnoel7@msn.com>
2021-05-31 16:58:58 +0100

    Changed implementation and layout of Music section

--- src/components/Pages/Music/Music.jsx
+++ src/components/Pages/Music/Music.jsx
@@ -1,49 +1,18 @@
 import './music.css';
+import songs from './songs';
 
 function Music() {
   return (
     <div className="music">
-      <div className="song">
-        <iframe title="Event Horizon - PTSD" width="80%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1030774990&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-        <div className="song-details">
-          <a href="https://soundcloud.com/thearchives_uk" className="channel-name" title="The Archives" target="_blank" rel="noreferrer">The Archives</a> · <a href="https://soundcloud.com/thearchives_uk/event-horizon-ptsd" className="artist-and-song-name" title="Event Horizon - PTSD" target="_blank" rel="noreferrer">Event Horizon - PTSD</a>
+      {songs.map((song, index) => (
+        <div className="song">
+          <iframe title={song.songTitle} width="300" height="300" scrolling="no" frameborder="no" allow="autoplay" src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${song.id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`}></iframe>
+          <div className="song-details">
+            <a href={song.channelUrl} className="channel-name" title={song.channelTitle} target="_blank" rel="noreferrer">{song.channelTitle}</a> · <a href={song.songUrl} className="artist-and-song-name" title={song.songTitle} target="_blank" rel="noreferrer">{song.songTitle}</a>
+          </div>
+          <br />
         </div>
-      </div>
-
-      <div className="song">
-        <iframe title="Event Horizon - Swindler" width="80%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/991349737&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-        <div className="song-details">
-          <a href="https://soundcloud.com/nightbassrecords" className="channel-name" title="Night Bass" target="_blank" rel="noreferrer">Night Bass</a> · <a href="https://soundcloud.com/nightbassrecords/event-horizon-swindler" className="artist-and-song-name" title="Event Horizon - Swindler" target="_blank" rel="noreferrer">Event Horizon - Swindler</a>
-        </div>
-      </div>
-
-      <div className="song">
-        <iframe title="Event Horizon - Crutch" width="80%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/991351594&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-        <div className="song-details">
-          <a href="https://soundcloud.com/nightbassrecords" className="channel-name" title="Night Bass" target="_blank" rel="noreferrer">Night Bass</a> · <a href="https://soundcloud.com/nightbassrecords/event-horizon-crutch" className="artist-and-song-name" title="Event Horizon - Crutch" target="_blank" rel="noreferrer">Event Horizon - Crutch</a>
-        </div>
-      </div>
-
-      <div className="song">
-        <iframe title="Event Horizon - Area Code (ft.PVC)" width="80%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/991352830&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-        <div className="song-details">
-          <a href="https://soundcloud.com/nightbassrecords" className="channel-name" title="Night Bass" target="_blank" rel="noreferrer">Night Bass</a> · <a href="https://soundcloud.com/nightbassrecords/event-horizon-area-code-ft-pvc" className="artist-and-song-name"  title="Event Horizon - Area Code (Ft. PVC)" target="_blank" rel="noreferrer">Event Horizon - Area Code (Ft. PVC)</a>
-        </div>
-      </div>
-
-      <div className="song">
-        <iframe title="Event Horizon - Intentions" width="80%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/991354150&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-        <div className="song-details">
-          <a href="https://soundcloud.com/nightbassrecords" className="channel-name" title="Night Bass" target="_blank" rel="noreferrer">Night Bass</a> · <a href="https://soundcloud.com/nightbassrecords/event-horizon-intentions" className="artist-and-song-name"  title="Event Horizon - Intentions" target="_blank" rel="noreferrer">Event Horizon - Intentions</a>
-        </div>
-      </div>
-
-      {/* <div className="song">
-        <iframe title="" width="80%" height="150" scrolling="no" frameborder="no" allow="autoplay" src=""></iframe>
-        <div className="song-details">
-          <a href="" className="channel-name" title="" target="_blank" rel="noreferrer"></a> · <a href="" className="artist-and-song-name"  title="" target="_blank" rel="noreferrer"></a>
-        </div>
-      </div> */}
+      ))}
     </div>    
   );
 }
