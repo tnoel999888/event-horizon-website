b827bf13925b4cdbb2c11cd783fa1b4a6c145afa
Tim <tnoel7@msn.com>
2021-11-21 22:31:13 +0000

    Fixing Insta api error handling

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const fetch = require("node-fetch");
 const port = process.env.PORT || 3002;
 const path = require('path');
+const e = require('express');
 
 require("dotenv").config();
 
@@ -15,7 +16,7 @@ const transport = {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
     }
-}
+};
 
 const transporter = nodemailer.createTransport(transport)
 
@@ -28,17 +29,17 @@ transporter.verify((error, success) => {
 });
 
 router.post('/api/send-email', (req, res, next) => {
-  const name = req.body.name
-  const email = req.body.email
-  const message = req.body.message
-  const content = `Name: ${name} \n\nEmail: ${email} \n\nMessage: ${message} `
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const content = `Name: ${name} \n\nEmail: ${email} \n\nMessage: ${message}`;
 
   const mail = {
     from: name,
     to: 'eventhorizonmusic@outlook.com',
     subject: 'Event Horizon Website Email',
     text: content
-  }
+  };
 
   transporter.sendMail(mail, (err, data) => {
     if (err) {
@@ -51,18 +52,25 @@ router.post('/api/send-email', (req, res, next) => {
       })
     }
   })
-})
+});
 
 router.get('/api/instagram/:after?', async (req, res) => {
   const { after } = req.params;
   const afterQuery = after ? `&after=${after}` : '';
   try {
     const response = await fetch(`https://graph.instagram.com/me/media?limit=24&fields=id,caption,permalink,media_type,media_url,thumbnail_url&access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}${afterQuery}`)
-    const json = await response.text();
-    return res.json({
-      success: true,
-      json
-    })
+    const responseText = await response.text();
+    if (typeof responseText === 'string') {
+      return res.status(500).json({
+        success: false,
+        message: "Error: Unable to fetch Instagram data",
+      })
+    } else {
+      return res.json({
+        success: true,
+        json: responseText,
+      })
+    }
   } catch (err) {
     return res.status(500).json({
       success: false,
@@ -71,10 +79,10 @@ router.get('/api/instagram/:after?', async (req, res) => {
   }
 })
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/', router)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -85,4 +93,4 @@ app.get('*', (req, res) => {
 app.listen(port, (err) => {
   if (err) return console.log(err);
   console.log('Server running on port: ', port);
-})
+});
