c3b45bf85cb23136260a2b38c419c7af5a0cdb4f
Tim <tnoel7@msn.com>
2022-07-30 00:20:23 +0100

    Fixing test

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -5,6 +5,6 @@ import App from "../App";
 test("Renders expected content", () => {
   const { container } = render(<App />);
   expect(container.getElementsByClassName("header").length).toBe(1);
-  expect(container.getElementsByClassName("app__page").length).toBe(1);
+  expect(container.getElementsByClassName("app").length).toBe(1);
   expect(container.getElementsByClassName("footer").length).toBe(1);
 });
